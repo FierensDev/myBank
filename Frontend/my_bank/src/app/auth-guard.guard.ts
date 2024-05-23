abec52234240fabaaeef4e73abdf3742d1c01aec
FierensDenis <denis.fierens@epitech.eu>
2024-05-23 05:22:27 +0200

    add rules routes

--- Frontend/my_bank/src/app/auth-guard.guard.ts
+++ Frontend/my_bank/src/app/auth-guard.guard.ts
@@ -1,37 +1,28 @@
-// import { CanActivateFn } from '@angular/router';
-
-// export const authGuardGuard: CanActivateFn = (route, state) => {
-//   return true;
-// };
-
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
 
 export const AuthGuard = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-    const router = inject(Router);
-    const token = true;
-    const currentUrl = state.url;
-    const myUrlsNoToken = ['/mybank/sign-in', '/mybank/sign-up', '/mybank/home'];
-    const myUrlsToken = ['/mybank/account', '/mybank/bank-transfer', '/mybank/user-settings', '/mybank/create-account'];
+  const router = inject(Router);
+  const token = false;
+  const currentUrl = state.url;
+  const myUrlsNoToken = ['/mybank/sign-in', '/mybank/sign-up', '/mybank/home'];
+  const myUrlsToken = ['/mybank/account', '/mybank/bank-transfer', '/mybank/user-settings', '/mybank/create-account'];
 
-    console.log(`URL  : `, router.url)
-    console.log(`deunsLog : `, currentUrl)
-    console.log(`deunsLog : `, document.location.href)
-
-    if(token){
-      if(myUrlsToken.includes(currentUrl)){
-        return true;
-      }
-      router.navigateByUrl('/mybank/account')
-      return false;
+  if(token){
+    if(myUrlsToken.includes(currentUrl)){
+      return true;
     }
+    router.navigateByUrl('/mybank/account')
+    return false;
+  }
 
-    // if(!token && mesUrlsNoToken.includes(currentUrl)){
-    //   return true;
-    // }
-    // if(token && mesUrlsToken.includes(currentUrl)){
-    //   router.navigateByUrl('/mybank/account')
-    //   return false;
-    // }
-    return true;
+  if(!token){
+    if(myUrlsNoToken.includes(currentUrl)){
+      return true;
+    }
+    router.navigateByUrl('/mybank/home')
+    return false;
+  }
+
+  return true;
 }
--- Frontend/my_bank/src/app/check-token/check-token.component.ts
+++ Frontend/my_bank/src/app/check-token/check-token.component.ts
@@ -12,30 +12,10 @@ import { NavbarComponent } from '../navbar/navbar.component';
 })
 export class CheckTokenComponent implements OnInit {
 
+  token = false;
   constructor(private router: Router, private cookieService: CookieService){}
 
   ngOnInit(): void {
-    // const token = false;
-
-    // if(token){
-    //   this.router.navigate(['/mybank/account']);
-    // }
-    // else {
-    //   this.router.navigate(['/mybank/home']);
-    // }
   }
 
-
-  // changeOfRoutes() {
-  //   console.log('Route changed to:');
-  //   // Ajoutez ici la logique spécifique que vous souhaitez exécuter
-  //   this.cookieService.set('SESSION_MYBANK', 'value');
-  //   let session_cookie = this.cookieService.get('SESSION_MYBANK');
-  //   console.log(`deunsLog : `, session_cookie);
-  //   if(session_cookie.length > 0){
-  //     console.log(`true : `, session_cookie );
-  //   } else {
-  //     console.log(`false : `, session_cookie )
-  //   }
-  // }
 }
